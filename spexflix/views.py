729e56619011c864c2bc50f3030ff6dc6447cb06
Karl Linderhed <dev@karlinde.se>
2024-12-14 19:29:19 +0100

    Add missing authentication to spexflix views
    In old site this was handled by the web server

--- spexflix/views.py
+++ spexflix/views.py
@@ -1,19 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from . import models, serializers
 
+@method_decorator(login_required, name='dispatch')
 class ProductionList(ListView):
     model = models.Production
     context_object_name = 'production_list'
     template_name = 'spexflix/production_list.html'
 
 
+@method_decorator(login_required, name='dispatch')
 class ProductionDetail(DetailView):
     model = models.Production
     context_object_name = 'production'
     template_name = 'spexflix/production_detail.html'
 
 
+@method_decorator(login_required, name='dispatch')
 class VideoDetail(DetailView):
     model = models.Video
     context_object_name = 'video'
@@ -22,4 +27,5 @@ class VideoDetail(DetailView):
 
 class ProductionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Production.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = serializers.ProductionSerializer
